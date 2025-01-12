5c711633174a5fb114fe345ade3345b6a6a2afa4
yxshwant <yashwantjen@gmail.com>
2025-01-13 00:39:01 +0530

    first commit

---
+++ clientside/src/data.js
@@ -0,0 +1,3 @@
+export let moviesList = ['Lucy', 'Life of Pie', 'Avengers', 'Jurassic World', 'Kill Bill', "The Nun", "The Martian", "The Japanese Wife"];
+export let slots = ['10:00 AM', '01:00 PM', '03:00 PM', "08:00 PM", "11:30 PM"];
+export let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2'];
